e219adc7018e5820e5c7cec04fd63de1d7d476db
Luke Davis <luke.davis@sanlam.co.za>
2025-06-29 01:19:38 +0200

    interface fix

--- backend/RetailBank/Repositories/ILedgerRepository.cs
+++ backend/RetailBank/Repositories/ILedgerRepository.cs
@@ -6,5 +6,5 @@ namespace RetailBank.Repositories;
 public interface ILedgerRepository
 {
     public Task CreateAccount(ulong accountNumber, LedgerAccountCode code, ulong userData64 = 0, UInt128? userData128 = null, uint userData32 = 0, uint ledger = 1, AccountFlags accountFlags = AccountFlags.None);
-    public Task Transfer(UInt128 id, ulong debitAccountId, ulong creditAccountId, UInt128 amount, uint ledger = 1, ushort code = 1, TransferFlags transferFlags = TransferFlags.None, ulong userData64=0, UInt128? pendingId = null);
+    public Task Transfer(UInt128 id, ulong debitAccountId, ulong creditAccountId, UInt128 amount, uint ledger = 1, TransferFlags transferFlags = TransferFlags.None, ushort code = 1, UInt64 userData64=0, UInt128? pendingId = null);
 }
