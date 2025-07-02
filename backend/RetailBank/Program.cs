85844e9fd65cc706c323cba9a2a5be3da6c62769
Ryan Christie <ryan.christie@bbd.co.za>
2025-07-02 22:20:37 +0200

    Removing cors

--- backend/RetailBank/Program.cs
+++ backend/RetailBank/Program.cs
@@ -30,13 +30,6 @@ builder.Services
 
     builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowFrontend", policy =>
-    {
-        policy.WithOrigins("http://localhost:5174") // <-- your frontend URL
-              .AllowAnyHeader()
-              .AllowAnyMethod();
-    });
-});
 
 var app = builder.Build();
 
@@ -48,7 +41,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.UseCors("AllowFrontend");
 
 app.AddEndpoints();
 
