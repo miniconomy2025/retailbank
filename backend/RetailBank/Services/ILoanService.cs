a5f65faba301e059d177d8b8736c1ba28d01b458
Luke Davis <luke.davis@sanlam.co.za>
2025-07-09 20:08:43 +0200

    Fix loan interface

--- backend/RetailBank/Services/ILoanService.cs
+++ backend/RetailBank/Services/ILoanService.cs
@@ -3,6 +3,5 @@ namespace RetailBank.Services;
 public interface ILoanService
 {
     Task<UInt128> CreateLoanAccount(UInt128 debitAccountNumber, ulong loanAmount);
-    Task ChargeInterest(UInt128 loanAccountId);
     Task PayInstallment(UInt128 loanAccountId);
 }
